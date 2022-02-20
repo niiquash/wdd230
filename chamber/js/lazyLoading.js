fd9426f5ed2cd1bcdb0f91ccc6b24a6e2104f4d9
niiquash <qua16006@byui.edu>
2022-02-19 19:04:29 -0700

    added local storage display of last visited on the discovery page.

--- chamber/js/lazyLoading.js
+++ chamber/js/lazyLoading.js
@@ -29,3 +29,27 @@ const imgObserver = new IntersectionObserver((entries, imgObserver) => {
 images.forEach(image => {
     imgObserver.observe(image);
 });
+
+// Display the amount of time in days
+//  Using local storage.
+
+// Get the first day.
+const day1 = new Date();
+const firstDay = day1.getDay();
+
+// Set firstday in local storage
+localStorage.setItem('firstDay', firstDay);
+console.log(localStorage)
+console.log(localStorage.getItem('firstDay'));
+
+// Get the current day of the user visit
+const userDay = new Date();
+let userDays = userDay.getDay();
+
+// Find the difference between the days.
+let fd = localStorage.getItem('firstDay');
+console.log(userDays - parseInt(fd))
+
+// Get the element in html and set its textContent to days elapsed.
+let daysElapsed = document.querySelector('.last-visited');
+daysElapsed.textContent = userDays - parseInt(fd);
