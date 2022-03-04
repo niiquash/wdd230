f4ffebcce577bc84d70284a2a1cce33a9f57314e
niiquash <qua16006@byui.edu>
2022-03-03 22:00:11 -0700

    added directory view buttons for grid/card and list views, with js dom manipulation to go with it.

--- chamber/js/directory.js
+++ chamber/js/directory.js
@@ -15,19 +15,43 @@ function displayBusinesses(business) {
     let biznesAddress = document.createElement("p");
     let biznesPhone = document.createElement("p");
     let biznesWebsite = document.createElement("a");
+    let biznesLogo = document.createElement("img");
 
     // Update the textContent of each new element
     biznesName.textContent = `${business.name}`;
+    biznesName.setAttribute("id", "business-name");
     biznesAddress.textContent = `${business.address}`;
     biznesPhone.textContent = `${business.phone}`;
+    biznesWebsite.textContent = `${business.website}`;
     biznesWebsite.setAttribute('href', business.website);
+    biznesLogo.setAttribute("src", business.imageurl);
+    biznesLogo.setAttribute("alt", `${business.name} logo`);
     
     // Appened list items in p-tags into the section/card
+    card.appendChild(biznesLogo)
     card.appendChild(biznesName);
     card.appendChild(biznesAddress);
     card.appendChild(biznesPhone);
     card.appendChild(biznesWebsite);
 
     // Append card to the existing div.
-    document.querySelector("div.cards").appendChild(card);
+    document.querySelector("div.list").appendChild(card);
+}
+
+const gridView = document.querySelector("#directory-card");
+const listView = document.querySelector("#directory-list");
+const currentDivView = document.querySelector(".list");
+
+gridView.addEventListener('click', togridView);
+
+function togridView() {
+    currentDivView.classList.remove("list");
+    currentDivView.classList.add("cards");
+}
+
+listView.addEventListener('click', tolistView);
+
+function tolistView() {
+    currentDivView.classList.remove("cards");
+    currentDivView.classList.add("list");
 }
--- lesson-09/prophet.js
+++ lesson-09/prophet.js
@@ -30,4 +30,5 @@ fetch(requestURL)
     document.querySelector('div.cards').appendChild(card);
 
   }
-
+  
+  
