63f27f2beda675f1c344e48e19ee2e3ff354565c
niiquash <qua16006@byui.edu>
2022-03-11 20:46:30 -0700

    created a lesson 10 folder. created a simple weather html and used javascript to display weather and weather icon.

---
+++ lesson-10/js/weather-api.js
@@ -0,0 +1,17 @@
+const apiURL = "https://api.openweathermap.org/data/2.5/weather?q=Fairbanks,us&units=imperial&appid=c515a57b84161fb3f31f685e290f0729";
+fetch(apiURL)
+    .then((response) => response.json())
+    .then((jsonObject) => {
+        console.log(jsonObject);
+        weather(jsonObject);
+    });
+    
+function weather(jsonObject) {
+    document.querySelector('#current-temp').textContent = jsonObject.main.temp
+    const iconsrc = `http://openweathermap.org/img/w/${jsonObject.weather[0].icon}.png`;
+    const desc = jsonObject.weather[0].description;
+    document.querySelector('#icon-src').textContent = iconsrc;
+    document.querySelector('#weathericon').setAttribute('src', iconsrc);
+    document.querySelector('#weathericon').setAttribute('alt', desc);
+    document.querySelector('figcaption').textContent = desc;
+}
